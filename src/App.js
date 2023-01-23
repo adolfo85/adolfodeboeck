42161d3b7b520411d926b33292a2145ff0a3da89
adolfo85 <adolfodeboeck@gmail.com>
2023-01-22 22:55:38 -0300

    creación del nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+       
         <h1>prueba</h1>
        
       </header>
