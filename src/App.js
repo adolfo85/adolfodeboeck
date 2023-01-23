6cfe96ab9199d69c8c7055ec184b0272fb46da2c
adolfo85 <adolfodeboeck@gmail.com>
2023-01-22 22:36:24 -0300

    prueba 2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-        
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>prueba</h1>
+       
       </header>
     </div>
   );
