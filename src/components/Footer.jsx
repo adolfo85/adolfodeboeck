da74032003665c6b0c0c3b8d30e7aee50e1febed
adolfo85 <adolfodeboeck@gmail.com>
2023-02-16 11:43:50 -0300

    eliminación de redes sociales de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,28 +7,10 @@ export default function Footer() {
   return (
     <MDBFooter bgColor='light' className='text-center text-lg-start text-muted footer'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+   
         
 
-        <div>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='facebook-f' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='twitter' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='google' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='instagram' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='linkedin' />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='github' />
-          </a>
-        </div>
+       
       </section>
 
       <section className=''>
