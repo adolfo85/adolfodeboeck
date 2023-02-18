15a959cd3d93fde1c905b25a67fd08e78ef3bb12
adolfo85 <adolfodeboeck@gmail.com>
2023-02-18 15:36:58 -0300

    actualización de artículo data fix

--- src/components/Articulodata.jsx
+++ src/components/Articulodata.jsx
@@ -23,8 +23,8 @@ function Articulo1() {
 <div class="d-flex position-relative">
   {/* <img src="..." class="flex-shrink-0 me-3" alt="..."> */}
   <div>
-    <h5 className="m-2 p-2">1- Análisis sobre el PBI, la pobreza, el déficit de infraestructura y los médicos per cápita en Argentina.</h5>
-    <p className = "m-2 p-2" >El siguiente análisis permite responder las siguientes preguntas: ¿Los bajos niveles de GDP están en relación directa al nivel de pobreza en las provincias argentinas?, ¿La deficiencia de infraestructura está en relación inversa a la cantidad de población?, ¿Existe relación entre el abandono escolar y los médicos per cápita en cada provincia?     
+    <h5 className="m-2 p-2">2- Análisis sobre el PBI, la pobreza, el déficit de infraestructura y los médicos per cápita en Argentina.</h5>
+    <p className = "m-2 p-2" >El siguiente análisis permite responder las siguientes preguntas: ¿Los bajos niveles de GDP están en relación directa al nivel de pobreza en las provincias argentinas?, ¿La deficiencia de infraestructura está en relación inversa a la cantidad de población?, ¿Existe relación entre el abandono escolar y los médicos per cápita en cada provincia?:      
     
     <a className = "m-4 p-3" href="https://deepnote.com/workspace/adolfo-003e916a-3c8a-4326-b1b3-26d1db77b68c/project/Argentina-086aa98e-b64a-4de0-9cc6-0e129ce0e61a/notebook/Notebook%201-3eaa39bf77ae4eb19d7d5f0cbc5495e5" class="stretched-link">Link</a>
     </p>
