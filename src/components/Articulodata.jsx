aa8186c54dea5cc799d93902bb3d088e00d2612c
adolfo85 <adolfodeboeck@gmail.com>
2023-03-29 13:33:00 -0300

    agregado de trabajo en data

--- src/components/Articulodata.jsx
+++ src/components/Articulodata.jsx
@@ -1,37 +1,71 @@
-
-
 function Articulo1() {
- 
-
   return (
     <>
+      <div class="d-flex position-relative">
+        {/* <img src="..." class="flex-shrink-0 me-3" alt="..."> */}
+        <div>
+          <h5 className="m-2 p-2">
+            1- Comparativa de ingresos generados por descarga y Streaming de
+            música
+          </h5>
+          <p className="m-2 p-2">
+            Los siguientes gráficos permiten comparar las ganancias generadas
+            por descargas y Streaming de música en nuestro país desde el año
+            2001 al 2020. La información fue obtenida a partir del Sistema de
+            Información Cultural de la Argentina (SINCA):
+            <a
+              className="m-4 p-3"
+              href="https://deepnote.com/workspace/adolfo-003e916a-3c8a-4326-b1b3-26d1db77b68c/project/GobData-f14f8beb-f869-4c35-a293-351aec8a7b1e/notebook/Notebook%201-51b4c9b2e05b448ca41fa7b320b085c3"
+            >
+              Link
+            </a>
+          </p>
+        </div>
+      </div>
 
-<div class="d-flex position-relative">
-  {/* <img src="..." class="flex-shrink-0 me-3" alt="..."> */}
-  <div>
-    <h5 className="m-2 p-2">1- Comparativa de ingresos generados por descarga y Streaming de música</h5>
-    <p className = "m-2 p-2" >Los siguientes gráficos permiten comparar las ganancias generadas por descargas y 
-    Streaming de música en nuestro país desde el año 2001 al 2020. La información fue obtenida 
-    a partir del Sistema de Información Cultural de la Argentina (SINCA): 
-    
-    <a className = "m-4 p-3" href="https://deepnote.com/workspace/adolfo-003e916a-3c8a-4326-b1b3-26d1db77b68c/project/GobData-f14f8beb-f869-4c35-a293-351aec8a7b1e/notebook/Notebook%201-51b4c9b2e05b448ca41fa7b320b085c3" class="stretched-link">Link</a>
-    </p>
-  </div>
-</div>
+      <div class="d-flex position-relative">
+        {/* <img src="..." class="flex-shrink-0 me-3" alt="..."> */}
+        <div>
+          <h5 className="m-2 p-2">
+            2- Análisis sobre el PBI, la pobreza, el déficit de infraestructura
+            y los médicos per cápita en Argentina.
+          </h5>
+          <p className="m-2 p-2">
+            El siguiente análisis permite responder las siguientes preguntas:
+            ¿Los bajos niveles de GDP están en relación directa al nivel de
+            pobreza en las provincias argentinas?, ¿La deficiencia de
+            infraestructura está en relación inversa a la cantidad de
+            población?, ¿Existe relación entre el abandono escolar y los médicos
+            per cápita en cada provincia?:
+            <a
+              className="m-4 p-3"
+              href="https://deepnote.com/workspace/adolfo-003e916a-3c8a-4326-b1b3-26d1db77b68c/project/Argentina-086aa98e-b64a-4de0-9cc6-0e129ce0e61a/notebook/Notebook%201-3eaa39bf77ae4eb19d7d5f0cbc5495e5"
+            >
+              Link
+            </a>
+          </p>
+        </div>
+      </div>
 
-
-<div class="d-flex position-relative">
-  {/* <img src="..." class="flex-shrink-0 me-3" alt="..."> */}
-  <div>
-    <h5 className="m-2 p-2">2- Análisis sobre el PBI, la pobreza, el déficit de infraestructura y los médicos per cápita en Argentina.</h5>
-    <p className = "m-2 p-2" >El siguiente análisis permite responder las siguientes preguntas: ¿Los bajos niveles de GDP están en relación directa al nivel de pobreza en las provincias argentinas?, ¿La deficiencia de infraestructura está en relación inversa a la cantidad de población?, ¿Existe relación entre el abandono escolar y los médicos per cápita en cada provincia?:      
-    
-    <a className = "m-4 p-3" href="https://deepnote.com/workspace/adolfo-003e916a-3c8a-4326-b1b3-26d1db77b68c/project/Argentina-086aa98e-b64a-4de0-9cc6-0e129ce0e61a/notebook/Notebook%201-3eaa39bf77ae4eb19d7d5f0cbc5495e5" class="stretched-link">Link</a>
-    </p>
-  </div>
-</div>
-    
-     
+      <div class="d-flex position-relative">
+        {/* <img src="..." class="flex-shrink-0 me-3" alt="..."> */}
+        <div>
+          <h5 className="m-2 p-2">
+            2- Visualizaciones sobre dataset shopcustomer{" "}
+          </h5>
+          <p className="m-2 p-2">
+            {" "}
+            Análisis de datos faltantes, eliminación de nulos y visualizaciones
+            varias:
+            <a
+              className="m-4 p-3"
+              href="https://vscode.dev/github/adolfo85/shopcustomer/blob/0e034e71ef274cbf8e46a9b33e064fd6394135ef"
+            >
+              Link
+            </a>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
