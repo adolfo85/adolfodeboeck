05f9ddd3555a886bd9192d240c8479e416e1ad35
adolfo85 <adolfodeboeck@gmail.com>
2023-06-22 11:48:37 -0300

    cambio de fuente fix2

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,31 +1,27 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../img/Logo2.png'
-// import About from '../pages/About';
-// import { NavLink } from "react-router-dom";
-// import { Link } from "react-router-dom";
-import './Nav.css'
-
+import logo from '../img/Logo2.png';
+import './Nav.css';
 
 function Navegador() {
   return (
-    <Navbar className='  mr-2 shadow-sm bg-white rounded  mb-2' bg="white" expand="lg" shadow>
-      <Container className='navcontainer' >
-      <Navbar.Brand href="#home">
-            <img
-              src= {logo}
-              width="40'"
-              height="40"
-              className="logo d-inline-block align-top "
-              alt="React Bootstrap logo"
-            />
-          </Navbar.Brand>
-        <Navbar.Brand href="/">Adolfo C. De Boeck  </Navbar.Brand>
+    <Navbar className='mr-2 shadow-sm bg-white rounded mb-2' bg="white" expand="lg" shadow>
+      <Container className='navcontainer'>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="40"
+            height="40"
+            className="logo d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Brand href="/">Adolfo C. De Boeck</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
+          <Nav className="me-auto text-center">
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href='/about'>About me</Nav.Link>
             <Nav.Link href="/dataanalytics">Data Analytics</Nav.Link>
             <Nav.Link href="/mern">MERN projects</Nav.Link>
@@ -33,7 +29,6 @@ function Navegador() {
             <Nav.Link href="/mern">Jazz guitar project</Nav.Link>
             <Nav.Link href="/mern">Other projects</Nav.Link>
             <Nav.Link href="/mern">Contact</Nav.Link>
-            
           </Nav>
         </Navbar.Collapse>
       </Container>
